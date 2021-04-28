4239e54ec87e2f47ff34908feac06adf14d9ec9b
camilabozo <66281792+camilabozo@users.noreply.github.com>
2021-04-28 16:48:28 -0300

    Add files via upload

---
+++ js/inicia.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.tabs').tabs();
+  $('.materialboxed').materialbox();
+  $('.carousel').carousel();
+  $('.sidenav').sidenav();
+  $('.collapsible').collapsible({
+    accordion: true
+  });
+});
+
+
